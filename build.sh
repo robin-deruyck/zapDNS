a7dd8536b66aa5aa97c724b785e039c32f45d16b
robin-deruyck <robin.2ruyck@gmail.com>
2024-10-15 03:37:16 +0200

    :recycle: update builder script

--- build.sh
+++ build.sh
@@ -2,15 +2,12 @@
 
 set -e
 
-rm -rf dist
-mkdir dist
-
 node --experimental-sea-config sea-config.json
 
 cd dist
 
 cp "$(command -v node)" zapdns.exe
 
-npx postject fdbdf NODE_SEA_BLOB dfbdf --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
+npx postject zapdns.exe NODE_SEA_BLOB zapdns.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
 
 echo "Build completed successfully."
